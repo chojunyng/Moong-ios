b4d4677c2ab3a160438153f9e34a3ac4827adeb2
BLU <pom0894@naver.com>
2018-01-28 04:42:22 +0900

    merge develop

---
+++ ThinkMemo/ThinkMemo/Keyword/CombineWordsVC.swift
@@ -0,0 +1,77 @@
+//
+//  CombineViewController
+//  ThinkMemo
+//
+//  Created by BLU on 2018. 1. 27..
+//  Copyright © 2018년 Unithon. All rights reserved.
+//
+
+import UIKit
+
+class CombineWordsVC: UIViewController {
+    
+    @IBOutlet var clearTextView: UITextView! {
+        didSet {
+            clearTextView.layer.cornerRadius = 30
+            clearTextView.layer.backgroundColor = UIColor.white.cgColor
+            clearTextView.layer.shadowColor = UIColor.darkGray.cgColor
+            clearTextView.layer.masksToBounds = false
+            clearTextView.layer.shadowOffset = CGSize(width: 0.1, height: 0.1)
+            clearTextView.layer.shadowOpacity = 0.1
+            clearTextView.layer.shadowRadius = 30
+        }
+    }
+    
+    @IBOutlet var wordsColvw: UICollectionView! {
+        didSet {
+            wordsColvw.dataSource = self
+            wordsColvw.register(UINib(nibName: "KeywordCell", bundle: nil), forCellWithReuseIdentifier: "KeywordCell")
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension CombineWordsVC : UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 24
+        let attributes : [NSAttributedStringKey : Any] = [
+            .paragraphStyle : style,
+            .font: UIFont(name: "Papya", size:20) ?? UIFont.systemFont(ofSize: 20)
+        ]
+        textView.attributedText = NSAttributedString(string: textView.text, attributes: attributes)
+    }
+}
+//extension CombineWordsVC : UITextViewDelegate {
+//    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+//        let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
+//        return newText.count <= 30
+//    }
+//}
+
+extension CombineWordsVC : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = wordsColvw.dequeueReusableCell(withReuseIdentifier: "KeywordCell", for: indexPath) as! KeywordCell
+        cell.backgroundColor = UIColor.init(hex: 0xffcd00)
+        
+        return cell
+    }
+}
+
---
+++ ThinkMemo/ThinkMemo/Keyword/KeywordVC.swift
@@ -0,0 +1,86 @@
+//
+//  KeywordVC
+//  ThinkMemo
+//
+//  Created by BLU on 2018. 1. 27..
+//  Copyright © 2018년 Unithon. All rights reserved.
+//
+
+import UIKit
+
+class KeywordVC : UIViewController {
+    
+    @IBOutlet var keywordColvw: UICollectionView! {
+        didSet {
+            keywordColvw.delegate = self
+            keywordColvw.dataSource = self
+            keywordColvw.register(UINib(nibName: reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
+        }
+    }
+    
+    let reuseIdentifier = "KeywordCell"
+    let MaximumNumberOfTaps = 3
+    var selectedCells = NSMutableArray()
+    var keywords = String()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        keywordColvw.allowsMultipleSelection = true
+    }
+
+}
+
+extension KeywordVC : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = keywordColvw.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! KeywordCell
+        
+        cell.isCellSelected = selectedCells.contains(indexPath)
+        
+        return cell
+    }
+    
+    // select, deselect cells
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard selectedCells.count < MaximumNumberOfTaps else { return }
+        selectedCells.add(indexPath)
+        collectionView.reloadItems(at: [indexPath])
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        selectedCells.remove(indexPath)
+        collectionView.reloadItems(at: [indexPath])
+
+    }
+}
+
+extension KeywordVC : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        guard collectionViewLayout is UICollectionViewFlowLayout else {
+            return .zero
+        }
+        let cellCount = CGFloat(collectionView.numberOfItems(inSection: section))
+        
+        return cellCount > 0 ? UIEdgeInsetsMake(26, 128.5, 0, 128.5) : .zero
+    }
+}
+
+extension UIColor {
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+}
