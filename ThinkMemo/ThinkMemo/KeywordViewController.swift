a9cdec246acb2240fcdb3a085c66e613a9ae8413
BLU <pom0894@naver.com>
2018-01-27 18:19:26 +0900

    keyword VC 레이아웃 추가

--- ThinkMemo/ThinkMemo/KeywordViewController.swift
+++ ThinkMemo/ThinkMemo/KeywordViewController.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2018년 Unithon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class KeywordViewController : UIViewController {
+    
+    @IBOutlet var keywordColvw: UICollectionView! {
+        didSet {
+            keywordColvw.delegate = self
+            keywordColvw.dataSource = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+extension KeywordViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = keywordColvw.dequeueReusableCell(withReuseIdentifier: "KeywordCell", for: indexPath)
+        
+        return cell
+    }
+}
+
+extension KeywordViewController : UICollectionViewDelegate {
+    
+}
