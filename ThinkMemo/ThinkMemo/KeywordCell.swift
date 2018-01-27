2155a4f4ef54abf01450610c746528be7d36f209
BLU <pom0894@naver.com>
2018-01-27 19:57:09 +0900

    keyword cell 추가 & multiselection 적용

---
+++ ThinkMemo/ThinkMemo/KeywordCell.swift
@@ -0,0 +1,14 @@
+//
+//  KeywordCell.swift
+//  ThinkMemo
+//
+//  Created by BLU on 2018. 1. 27..
+//  Copyright © 2018년 Unithon. All rights reserved.
+//
+
+import UIKit
+
+class KeywordCell: UICollectionViewCell {
+    @IBOutlet var title: UILabel!
+    @IBOutlet var checkImage: UIImageView!
+}
--- ThinkMemo/ThinkMemo/KeywordViewController.swift
+++ ThinkMemo/ThinkMemo/KeywordViewController.swift
@@ -14,9 +14,22 @@ class KeywordViewController : UIViewController {
         didSet {
             keywordColvw.delegate = self
             keywordColvw.dataSource = self
+            keywordColvw.allowsMultipleSelection = true
+            
         }
     }
     
+    var selectedCells : NSMutableArray = []
+    var keywords : [String] = []
+    let reuseIdentifier = "KeywordCell"
+    
+//    var isCellSelected: Bool {
+//
+//    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -30,10 +43,33 @@ extension KeywordViewController : UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = keywordColvw.dequeueReusableCell(withReuseIdentifier: "KeywordCell", for: indexPath)
+        let cell = keywordColvw.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! KeywordCell
+        cell.layer.cornerRadius = 10
+        
+        if selectedCells.contains(indexPath) {
+            cell.isSelected = true
+            cell.checkImage.isHidden = false
+        }
+        else{
+            cell.isSelected = false
+            cell.checkImage.isHidden = true
+        }
         
         return cell
     }
+    
+    // select, deselect cells
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("NO")
+        selectedCells.add(indexPath)
+        keywordColvw.reloadItems(at: [indexPath])
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        selectedCells.remove(indexPath)
+        keywordColvw.reloadItems(at: [indexPath])
+    }
+    
 }
 
 extension KeywordViewController : UICollectionViewDelegate {
